93eda51d08f6cba9f80129fd949cfe62ac36a569
hypocrisy-ywk <1145026809@qq.com>
2020-05-27 20:02:03 +0800

    chushi

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    title: 'Learning Notes',
+    description:'我的个人博客',
+    themeConfig: {
+        logo: '/study.jpg',
+        nav: [{
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: 'study-ui',
+                link: 'https://hypocrisy-ywk.github.io/study-ui/'
+            },
+            {
+                text: 'financial',
+                link: '/financial/'
+            },
+            {
+                text: 'notes',
+                link: '/notes/'
+            },
+            {
+                text: 'other',
+                link: '/other/'
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/hypocrisy-ywk'
+            },
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /study.jpg
+heroText: Learning Notes
+tagline: Feel Free To What You Learning
+actionText: Get Started →
+actionLink: /zh/guide/
+features:
+- title: Knowledge point
+  details: Summarized some front-end development skills and knowledge points, my study notes and memos.
+- title: Difficult point
+  details: I recorded the problems I encountered in my study or project and the solutions.
+footer: Let me dive into it!!!
+---
